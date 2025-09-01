ed83eb83c3e0610a3eb96d0fbc6cbba8c90d0758
Pedro Henrique da Silva Soares <37024908@sempreunijuazeiro.com.br>
2025-09-01 20:18:17 -0300

    add if else para o nome carla

--- array.py
+++ array.py
@@ -1,6 +1,16 @@
-minha_lista = ["pedro", "carla", "isaac", "samuel", "paulo"]
-
+minha_lista = ["Pedro", "Carla", "Isaac", "Samuel", "Paulo"]
+nome = "Carla"
 
 print("Primeiro nome:", minha_lista[0])
 print("Terceiro nome:", minha_lista[-3])
 print("Ultimo nome:", minha_lista[-1])
+print("  ")
+
+if nome in minha_lista:
+    print(f"{nome} está na lista!")
+        
+else:
+    print("Carla não foi encontrada na lista.")
+
+
+
